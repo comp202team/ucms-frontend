178e37f37bac071d0c279cbbe33c15f4240fb383
BBerkeYildirim <batuhanberke.yildirim@gmail.com>
2023-03-28 18:41:12 +0300

    Redux store and axios instance created

---
+++ src/Libs/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+
+
+const ProtectedRoute =(props : any) => {
+    const dispatch = useDispatch()
+
+    return (
+        <div></div>
+    );
+}
+
+export default ProtectedRoute;
